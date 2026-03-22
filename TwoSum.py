9020d02ded626d2cd28574fd482f152bd4a901c3
Soumajit Majumder <majumdersoumajit09@gmail.com>
2026-03-22 18:52:16 +0530

    add optimised approach

--- TwoSum.py
+++ TwoSum.py
@@ -1,14 +1,25 @@
-def twoSum(nums, target):
+# bruteforce approach
+def twoSum1(nums, target):
         n = len(nums)
         for i in range(n-1):
             for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-
+# optimised approach
+def twoSum2( nums, target):
+        n = len(nums)
+        hashMap = {}
+        for i in range(n):
+            hashMap[nums[i]] = i
+        
+        for i in range(n):
+            needed = target - nums[i]
+            if needed in hashMap and hashMap[needed] != i:
+                return [i, hashMap[needed]]
 
 
 
 arr = list(map(int, input().split()))
 target = int(input())
-print(twoSum(arr, target))
+print(twoSum2(arr, target))
